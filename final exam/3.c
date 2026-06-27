f31da06bb7eb524b84be0ac77583c166767e6820
RawalTarun <rawaltarun45@gmail.com>
2026-06-27 19:17:50 +0530

    Add files via upload

---
+++ final exam/3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void main()
+{
+
+    char a[50], y;
+    int len = 0,
+
+        printf("enter your name:");
+    scanf("%s", a);
+
+    while (a[len] != '\0')
+    {
+        len++;
+    }
+    for (int i = 0; i = len / 2; i++)
+    {
+        y = a[i];
+        a[i] = a[len - 1 - i];
+        a[len - 1 - i] = y;
+    }
+    printf("revered string=%s", a);
+}
